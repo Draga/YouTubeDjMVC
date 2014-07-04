1fb4c9f6c4145f59f156330419e6cf535fade5f3
Andrew Murphy <andrewm1986@gmail.com>
2014-07-04 09:47:11 +0100

    Make /player work

--- YouTubeDjMVC/App_Start/RouteConfig.cs
+++ YouTubeDjMVC/App_Start/RouteConfig.cs
@@ -12,10 +12,10 @@ namespace YouTubeDjMVC
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-            
+
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
+                url: "{action}/{id}",
                 defaults: new { controller = "Video", action = "Index", id = UrlParameter.Optional }
             );
         }
